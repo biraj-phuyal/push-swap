49c714b19b96f31db909934e8231337718a31f04
biraj-phuyal <birajphuyal11@gmail.com>
2025-10-26 16:02:42 +0000

    reverse rotate

--- core/reverse_rotate.c
+++ core/reverse_rotate.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 15:39:42 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/10/26 15:40:37 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/10/26 16:02:07 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 
 void reverse_rotate(t_stack *stack)
 {
-	
+	if(!stack->head || !stack->head->next)
+	stack->head->val = stack->tail->val;
+	stack->tail->val = stack->head->val;
+	stack->tail->next = stack->head;
+	stack->head->prev = stack->tail;
+}
+
+void rra(t_stack *stack_a)
+{
+	reverse_rotate(stack_a);
+	write(1, "rra\n", 4);
+}
+
+void rrb(t_stack *stack_b)
+{
+	reverse_rotate(stack_b);
+	write(1, "rrb\n", 4);
+}
+
+void rra(t_stack *stack_a, t_stack *stack_b)
+{
+	reverse_rotate(stack_a);
+	reverse_rotate(stack_b);
+	write(1, "rrr\n", 4);
 }
