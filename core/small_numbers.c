db2faaa9a853023f9c753963fe14a6258a606049
biraj-phuyal <birajphuyal11@gmail.com>
2025-10-27 18:49:38 +0000

    three sort

--- core/small_numbers.c
+++ core/small_numbers.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 21:26:07 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/10/26 14:35:27 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/10/27 18:48:44 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,24 @@ void	sort_three_numbers(t_stack *stack_a)
 	int	first;
 	int	second;
 	int	third;
+
+	first = stack_a->head->val;
+	second = stack_a->head->next->val;
+	third = stack_a->tail->val;
+	if (first > second && second < third)
+		sa(stack_a);
+	else if (first > second && first > third)
+		ra(stack_a);
+	else if (second > first && first < third)
+	{
+		sa(stack_a);
+		ra(stack_a);
+	}
+	else if (first > second && second > third)
+	{
+		sa(stack_a);
+		rra(stack_a);
+	}
+	else
+		rra(stack_a);
 }
