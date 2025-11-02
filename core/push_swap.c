0ba45357f2b4d01606b52b7c3ae742df1b616b4e
biraj-phuyal <birajphuyal11@gmail.com>
2025-11-02 18:32:51 +0000

    Leakproof

--- core/push_swap.c
+++ core/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 14:05:54 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/11/01 22:56:37 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/11/02 17:22:28 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,20 @@ void	process_indexing(t_stack *stack)
 	}
 }
 
+void	call_sorts(t_stack *stack_a, t_stack *stack_b)
+{
+	if (stack_a->size == 2)
+		sa(stack_a);
+	else if (stack_a->size == 3)
+		sort_three_numbers(stack_a);
+	else if (stack_a->size == 4)
+		sort_four_numbers(stack_a, stack_b);
+	else if (stack_a->size == 5)
+		sort_five_numbers(stack_a, stack_b);
+	else
+		radix_sort(stack_a, stack_b);
+}
+
 int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
@@ -48,20 +62,13 @@ int	main(int argc, char **argv)
 	stack_a = ft_calloc(sizeof(t_stack), 1);
 	stack_b = ft_calloc(sizeof(t_stack), 1);
 	if (!start(stack_a, argc, argv))
-		exit(write(2, "Error\n", 6));
-	if (!ordered(stack_a))
 	{
-		if (stack_a->size == 2)
-			sa(stack_a);
-		else if (stack_a->size == 3)
-			sort_three_numbers(stack_a);
-		else if (stack_a->size == 4)
-			sort_four_numbers(stack_a, stack_b);
-		else if (stack_a->size == 5)
-			sort_five_numbers(stack_a, stack_b);
-		else
-			radix_sort(stack_a, stack_b);
+		free(stack_a);
+		free(stack_b);
+		exit(write(2, "Error\n", 6));
 	}
+	if (!ordered(stack_a))
+		call_sorts(stack_a, stack_b);
 	free_stack(stack_a);
 	free_stack(stack_b);
 	free(stack_a);
--- core/radix_sort.c
+++ core/radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 18:13:46 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/10/29 17:41:12 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/11/02 17:18:33 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ void	radix_sort(t_stack *stack_a, t_stack *stack_b)
 		while (size--)
 		{
 			idx = stack_a->head->idx;
-			if (((idx >> i) & 1) & 1)
+			if ((idx >> i) & 1)
 				ra(stack_a);
 			else
 				pb(stack_b, stack_a);
